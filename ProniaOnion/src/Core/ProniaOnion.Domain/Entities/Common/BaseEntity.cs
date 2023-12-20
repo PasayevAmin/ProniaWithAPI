409f881d95536feff3a75664e848c7a8e6288048
Your Name <you@example.com>
2023-12-20 06:27:39 +0400

    Onion Arxitecture

---
+++ ProniaOnion/src/Core/ProniaOnion.Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProniaOnion.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public DateTime CreateAt { get; set; }
+        public DateTime? ModifieldAt { get; set; }
+        public string CreatedBy { get; set; } = null!;
+        public BaseEntity()
+        {
+            CreatedBy = "amin.pasayev";
+            CreateAt = DateTime.Now;
+        }
+    }
+}
---
+++ ProniaOnion/src/Infrastructure/ProniaOnion.Persistence/DAL/AppDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProniaOnion.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProniaOnion.Persistence.DAL
+{
+    internal class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Category> MyProperty { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<ProductColor> ProductColors { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().Property(x => x.Name).IsRequired().HasMaxLength(50);
+            modelBuilder.Entity<Product>().Property(x => x.Name).IsRequired().HasMaxLength(50);
+            modelBuilder.Entity<Product>().Property(x => x.Price).IsRequired().HasColumnType("decimal(6,2)");
+            modelBuilder.Entity<Product>().Property(x => x.Description).IsRequired(false).HasColumnType("text");
+            modelBuilder.Entity<Product>().Property(x => x.SKU).IsRequired().HasMaxLength(100);
+            modelBuilder.Entity<Color>().Property(x => x.Name).IsRequired().HasMaxLength(50);
+
+
+
+
+
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+
+    }
+}
